ded173a3886bd55f94086e76012b15e673531f2f
Arkadii Haidarzhy <arkadiy@byzantium.com.ua>
2024-02-14 12:05:51 +0200

    challenge #6

--- myApp.js
+++ myApp.js
@@ -1,50 +1,24 @@
 let express = require('express');
 let app = express();
+require('dotenv').config()
+
+console.log()
 
 app.use('/public', express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
- res.sendFile(__dirname + '/views/index.html')
+    res.sendFile(__dirname + '/views/index.html')
 })
 
 app.get("/json", (req, res) => {
- res.json({"message": "Hello json"});
+    const data = {"message": "Hello json"};
+
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        data.message = data.message.toUpperCase();
+    }
+
+    res.json(data);
 })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
